53815af36813f5f2f8b1a985f13cfde6b8fa698b
webfinesse <webfinesse@gmail.com>
2017-12-10 15:34:12 -0500

    Turning off exceptions in the engine dll

---
+++ VeritasEngine/BoostExceptionHandler.cpp
@@ -0,0 +1,6 @@
+#include "BoostExceptionHandler.h"
+
+void boost::throw_exception(std::exception const & e)
+{
+	
+}
---
+++ VeritasEngine/BoostExceptionHandler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <exception>
+
+namespace boost
+{
+#ifdef BOOST_NO_EXCEPTIONS
+	void throw_exception(std::exception const & e);
+#endif
+}
